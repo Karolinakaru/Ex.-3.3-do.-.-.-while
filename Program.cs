07865e449273d3155ab56ce9fb999e71a2b5fed6
singhet <singhet@SCSWL4072>
2024-01-19 17:53:17 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int number = 1;
+        Console.WriteLine("Consecutive integers from 1 to 20 and squared value of each");
+        do
+        {
+            int squaredNumber = number * number;
+            Console.WriteLine($"{number}^ = {squaredNumber}");
+            number++;
+        } while (number <= 20);
+        Console.ReadKey();
+    }
+
+}
